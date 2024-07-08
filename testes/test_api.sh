cbbbd56cdcf83f9af3b0a1f5c9ace69cae9d4942
Luis Fernando do Rosario Cintra <82674962+fernandocintra2871@users.noreply.github.com>
2024-07-08 14:29:36 -0300

    Pequena alteração nos testes

--- testes/test_api.sh
+++ testes/test_api.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 # URLs da API
-BRASIL_URL="http://127.0.0.1:12345"
-BRADESCO_URL="http://127.0.0.1:12346"
-CAIXA_URL="http://127.0.0.1:12347"
+BRASIL_URL="http://ip:12345"
+BRADESCO_URL="http://ip:12346"
+CAIXA_URL="http://ip:12347"
 
 # Nome dos Bancos
 BRASIL="brasil"
 BRADESCO="bradesco"
 CAIXA="caixa"
 
+# Arquivos de cookies
+COOKIES_BRASIL="cookies_brasil.txt"
+COOKIES_BRADESCO="cookies_bradesco.txt"
+COOKIES_CAIXA="cookies_caixa.txt"
+
 # Usuários de teste
-USER1="user1"
+USER1="user1123"
 PASS1="password1"
-USER2="user2"
+USER2="user2223"
 PASS2="password2"
-USER3="user3"
+USER3="user3323"
 PASS3="password3"
 
 # Função para registrar um usuário
@@ -33,7 +38,9 @@ login_user() {
   local username=$1
   local password=$2
   local bank_url=$3
-  curl -s -X POST -H "Content-Type: application/json" -c cookies.txt \
+  local cookies_file=$4
+
+  curl -s -X POST -H "Content-Type: application/json" -c "$cookies_file" \
     -d "{\"username\":\"$username\",\"password\":\"$password\"}" \
     "$bank_url/login"
 }
@@ -43,18 +50,11 @@ deposit() {
   local amount=$1
   local bank_name=$2
   local bank_url=$3
-  curl -s -X POST -H "Content-Type: application/json" -b cookies.txt \
+  local cookies_file=$4
+
+  curl -s -X POST -H "Content-Type: application/json" -b "$cookies_file" \
     -d "{\"bank\":\"$bank_name\", \"joint_account\":false, \"second_holder\":\"\", \"amount\":$amount}" \
     "$bank_url/deposit_op"
-}
-
-# Função para fazer saque
-withdraw() {
-  local amount=$1
-  local bank_url=$2
-  curl -s -X POST -H "Content-Type: application/json" -b cookies.txt \
-    -d "{\"bank\":\"brasil\", \"joint_account\":false, \"second_holder\":\"\", \"amount\":$amount}" \
-    "$bank_url/withdraw_op"
 }
 
 # Função para fazer transferência
@@ -64,32 +64,27 @@ transfer() {
   local amount=$3
   local bank_name=$4
   local bank_url=$5
-  curl -s -X POST -H "Content-Type: application/json" -b cookies.txt \
+local cookies_file=$6
+  curl -s -X POST -H "Content-Type: application/json" -b "$cookies_file" \
     -d "[{\"user_id\":\"$origin_username\", \"bank\":\"$bank_name\", \"amount\":$amount}, {\"user_id\":\"$target_username\", \"bank\":\"brasil\", \"amount\":$amount}]" \
     "$bank_url/transfer_op"
-}
-
-# Função para fazer pagamento
-payment() {
-  local user_id=$1
-  local amount=$2
-  local bank_url=$3
-  curl -s -X POST -H "Content-Type: application/json" -b cookies.txt \
-    -d "[{\"user_id\":\"$user_id\", \"bank\":\"brasil\", \"amount\":$amount}]" \
-    "$bank_url/payment_op"
 }
 
 # Função para obter saldos
 get_balances() {
   local bank_url=$1
-  curl -s -X GET -H "Content-Type: application/json" -b cookies.txt \
+  local cookies_file=$2
+
+  curl -s -X GET -H "Content-Type: application/json" -b "$cookies_file" \
     "$bank_url/ab_balance"
 }
 
 # Função para fazer logout
 logout() {
   local bank_url=$1
-  curl -s -X POST -H "Content-Type: application/json" -b cookies.txt \
+  local cookies_file=$2
+
+  curl -s -X POST -H "Content-Type: application/json" -b "$cookies_file" \
     "$bank_url/logout"
 }
 
@@ -100,36 +95,129 @@ register_user $USER1 $PASS1 $BRADESCO_URL
 register_user $USER1 $PASS1 $CAIXA_URL
 register_user $USER2 $PASS2 $BRASIL_URL
 
-# Login com o primeiro usuário
+# Login com o primeiro usuário em cada banco
 echo "Logando com $USER1..."
-login_user $USER1 $PASS1 $BRASIL_URL
-login_user $USER1 $PASS1 $BRADESCO_URL
-login_user $USER1 $PASS1 $CAIXA_URL
+login_user $USER1 $PASS1 $BRASIL_URL $COOKIES_BRASIL
+login_user $USER1 $PASS1 $BRADESCO_URL $COOKIES_BRADESCO
+login_user $USER1 $PASS1 $CAIXA_URL $COOKIES_CAIXA
 
-# Depositar dinheiro
+# Depositar dinheiro em cada banco
 echo "Depositando dinheiro..."
-deposit 500 $BRASIL $BRASIL_URL
-deposit 500 $BRADESCO $BRADESCO_URL
-deposit 500 $CAIXA $CAIXA_URL
+deposit 500 $BRASIL $BRASIL_URL $COOKIES_BRASIL
+deposit 500 $BRADESCO $BRADESCO_URL $COOKIES_BRADESCO
+deposit 500 $CAIXA $CAIXA_URL $COOKIES_CAIXA
 
-#echo "Sacando dinheiro..."
-# Fazer saques
-#withdraw 100 $BRASIL
+# Esperar por todas as operações terminarem
+wait
 
-#echo "Transferindo dinheiro..."
-# Transferir dinheiro
-#transfer $USER1 $USER2 100 
+# Obtendo saldos de cada banco
+echo "Obtendo saldos..."
+get_balances $BRASIL_URL $COOKIES_BRASIL
+get_balances $BRADESCO_URL $COOKIES_BRADESCO
+get_balances $CAIXA_URL $COOKIES_CAIXA
 
-#echo "Realizando pagamento..."
-# Realizar pagamentos simultâneos
-#payment $USER1 100
+# Realizar operações simultâneas
+echo "Realizando operações simultâneas..."
+
+transfer $USER1 $USER2 10 $BRASIL $BRASIL_URL $COOKIES_BRASIL &
+transfer $USER1 $USER2 10 $BRASIL $BRADESCO_URL $COOKIES_BRADESCO &
+transfer $USER1 $USER2 10 $BRASIL $CAIXA_URL $COOKIES_CAIXA &
+transfer $USER1 $USER2 10 $BRASIL $BRASIL_URL $COOKIES_BRASIL &
+transfer $USER1 $USER2 10 $BRASIL $BRADESCO_URL $COOKIES_BRADESCO &
+transfer $USER1 $USER2 10 $BRASIL $CAIXA_URL $COOKIES_CAIXA &
+transfer $USER1 $USER2 10 $BRASIL $BRASIL_URL $COOKIES_BRASIL &
+transfer $USER1 $USER2 10 $BRASIL $BRADESCO_URL $COOKIES_BRADESCO &
+transfer $USER1 $USER2 10 $BRASIL $CAIXA_URL $COOKIES_CAIXA &
+
+transfer $USER1 $USER2 10 $BRASIL $BRASIL_URL $COOKIES_BRASIL &
+transfer $USER1 $USER2 10 $BRASIL $BRADESCO_URL $COOKIES_BRADESCO &
+transfer $USER1 $USER2 10 $BRASIL $CAIXA_URL $COOKIES_CAIXA &
+transfer $USER1 $USER2 10 $BRASIL $BRASIL_URL $COOKIES_BRASIL &
+transfer $USER1 $USER2 10 $BRASIL $BRADESCO_URL $COOKIES_BRADESCO &
+transfer $USER1 $USER2 10 $BRASIL $CAIXA_URL $COOKIES_CAIXA &
+transfer $USER1 $USER2 10 $BRASIL $BRASIL_URL $COOKIES_BRASIL &
+transfer $USER1 $USER2 10 $BRASIL $BRADESCO_URL $COOKIES_BRADESCO &
+transfer $USER1 $USER2 10 $BRASIL $CAIXA_URL $COOKIES_CAIXA &
+
+transfer $USER1 $USER2 10 $BRASIL $BRASIL_URL $COOKIES_BRASIL &
+transfer $USER1 $USER2 10 $BRASIL $BRADESCO_URL $COOKIES_BRADESCO &
+transfer $USER1 $USER2 10 $BRASIL $CAIXA_URL $COOKIES_CAIXA &
+transfer $USER1 $USER2 10 $BRASIL $BRASIL_URL $COOKIES_BRASIL &
+transfer $USER1 $USER2 10 $BRASIL $BRADESCO_URL $COOKIES_BRADESCO &
+transfer $USER1 $USER2 10 $BRASIL $CAIXA_URL $COOKIES_CAIXA &
+transfer $USER1 $USER2 10 $BRASIL $BRASIL_URL $COOKIES_BRASIL &
+transfer $USER1 $USER2 10 $BRASIL $BRADESCO_URL $COOKIES_BRADESCO &
+transfer $USER1 $USER2 10 $BRASIL $CAIXA_URL $COOKIES_CAIXA &
+
+transfer $USER1 $USER2 10 $BRASIL $BRASIL_URL $COOKIES_BRASIL &
+transfer $USER1 $USER2 10 $BRASIL $BRADESCO_URL $COOKIES_BRADESCO &
+transfer $USER1 $USER2 10 $BRASIL $CAIXA_URL $COOKIES_CAIXA &
+transfer $USER1 $USER2 10 $BRASIL $BRASIL_URL $COOKIES_BRASIL &
+transfer $USER1 $USER2 10 $BRASIL $BRADESCO_URL $COOKIES_BRADESCO &
+transfer $USER1 $USER2 10 $BRASIL $CAIXA_URL $COOKIES_CAIXA &
+transfer $USER1 $USER2 10 $BRASIL $BRASIL_URL $COOKIES_BRASIL &
+transfer $USER1 $USER2 10 $BRASIL $BRADESCO_URL $COOKIES_BRADESCO &
+transfer $USER1 $USER2 10 $BRASIL $CAIXA_URL $COOKIES_CAIXA &
+
+transfer $USER1 $USER2 10 $BRASIL $BRASIL_URL $COOKIES_BRASIL &
+transfer $USER1 $USER2 10 $BRASIL $BRADESCO_URL $COOKIES_BRADESCO &
+transfer $USER1 $USER2 10 $BRASIL $CAIXA_URL $COOKIES_CAIXA &
+transfer $USER1 $USER2 10 $BRASIL $BRASIL_URL $COOKIES_BRASIL &
+transfer $USER1 $USER2 10 $BRASIL $BRADESCO_URL $COOKIES_BRADESCO &
+transfer $USER1 $USER2 10 $BRASIL $CAIXA_URL $COOKIES_CAIXA &
+transfer $USER1 $USER2 10 $BRASIL $BRASIL_URL $COOKIES_BRASIL &
+transfer $USER1 $USER2 10 $BRASIL $BRADESCO_URL $COOKIES_BRADESCO &
+transfer $USER1 $USER2 10 $BRASIL $CAIXA_URL $COOKIES_CAIXA &
+
+transfer $USER1 $USER2 10 $BRASIL $BRASIL_URL $COOKIES_BRASIL &
+transfer $USER1 $USER2 10 $BRASIL $BRADESCO_URL $COOKIES_BRADESCO &
+transfer $USER1 $USER2 10 $BRASIL $CAIXA_URL $COOKIES_CAIXA &
+transfer $USER1 $USER2 10 $BRASIL $BRASIL_URL $COOKIES_BRASIL &
+transfer $USER1 $USER2 10 $BRASIL $BRADESCO_URL $COOKIES_BRADESCO &
+transfer $USER1 $USER2 10 $BRASIL $CAIXA_URL $COOKIES_CAIXA &
+transfer $USER1 $USER2 10 $BRASIL $BRASIL_URL $COOKIES_BRASIL &
+transfer $USER1 $USER2 10 $BRASIL $BRADESCO_URL $COOKIES_BRADESCO &
+transfer $USER1 $USER2 10 $BRASIL $CAIXA_URL $COOKIES_CAIXA &
+
+transfer $USER1 $USER2 10 $BRASIL $BRASIL_URL $COOKIES_BRASIL &
+transfer $USER1 $USER2 10 $BRASIL $BRADESCO_URL $COOKIES_BRADESCO &
+transfer $USER1 $USER2 10 $BRASIL $CAIXA_URL $COOKIES_CAIXA &
+transfer $USER1 $USER2 10 $BRASIL $BRASIL_URL $COOKIES_BRASIL &
+transfer $USER1 $USER2 10 $BRASIL $BRADESCO_URL $COOKIES_BRADESCO &
+transfer $USER1 $USER2 10 $BRASIL $CAIXA_URL $COOKIES_CAIXA &
+transfer $USER1 $USER2 10 $BRASIL $BRASIL_URL $COOKIES_BRASIL &
+transfer $USER1 $USER2 10 $BRASIL $BRADESCO_URL $COOKIES_BRADESCO &
+transfer $USER1 $USER2 10 $BRASIL $CAIXA_URL $COOKIES_CAIXA &
 
 wait
 
+# Obtendo saldos de cada banco
 echo "Obtendo saldos..."
-get_balances $BRASIL_URL
-get_balances $BRADESCO_URL
-get_balances $CAIXA_URL
+get_balances $BRASIL_URL $COOKIES_BRASIL
+get_balances $BRADESCO_URL $COOKIES_BRADESCO
+get_balances $CAIXA_URL $COOKIES_CAIXA
+
+# Saindo das contas cujo valor foi transferido
+echo "Deslogando..."
+logout $BRASIL_URL $COOKIES_BRASI
+logout $BRADESCO_URL $COOKIES_BRADESCO 
+logout $CAIXA_URL $COOKIES_CAIXA
+ 
+wait
+
+echo "Login na conta 2..."
+login_user $USER2 $PASS2 $BRASIL_URL $COOKIES_BRASIL
+
+echo "Obtendo saldos..."
+get_balances $BRASIL_URL $COOKIES_BRASIL
+
+echo "Deslogando..."
+logout $BRASIL_URL $COOKIES_BRASI
+
+# Exemplo de logout (opcional)
+# logout $BRASIL_URL $COOKIES_BRASIL
+# logout $BRADESCO_URL $COOKIES_BRADESCO
+# logout $CAIXA_URL $COOKIES_CAIXA
 
 echo "Contas criadas e saldos adicionados."
 
